8843b654e1bac75850d25b36eb582c215d799c02
Sankrita Patel <sankrita6960@outlook.com>
2024-05-27 21:20:51 +0530

    Data Transformation Done

--- src/pneumoniaDisease/constants/constant/__init__.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-TIMESTAMP: datetime = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
-
-# Data Ingestion Constants
-ARTIFACT_DIR: str = "artifacts"
-BUCKET_NAME: str = 'xraylungmages'
-S3_DATA_FOLDER: str = 'data'
--- src/pneumoniaDisease/pipelines/training_pipeline.py
+++ src/pneumoniaDisease/pipelines/training_pipeline.py
@@ -1,33 +1,85 @@
 import sys
+from src.pneumoniaDisease.components.data_transformation import DataTransformation
 from src.pneumoniaDisease.components.data_ingestion import DataIngestion
-from src.pneumoniaDisease.entity.artifact_entity import(DataIngestionArtifact)
-from src.pneumoniaDisease.entity.config_entity import (DataIngestionConfig)
+from src.pneumoniaDisease.entity.artifact_entity import(DataIngestionArtifact, DataTransformationArtifact)
+from src.pneumoniaDisease.entity.config_entity import (DataIngestionConfig, DataTransformationConfig)
 from src.pneumoniaDisease.exception import CustomException
 from src.pneumoniaDisease.logger import logging
 
 class TrainPipeline:
-    def __init__(self) -> None:
+    def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_transformation_config = DataTransformationConfig()
 
 
-    def start_data_ingestion(self)->DataIngestionArtifact:
+
+      
+    def start_data_ingestion(self) -> DataIngestionArtifact:
+        logging.info("Entered the start_data_ingestion method of TrainPipeline class")
         try:
-            logging.info("Entered the start_data_ingestion method od TrainPipeline class")
-            logging.info("Gettign the data from s3 bucket")
-            data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
+
+            logging.info("Getting the data from s3 bucket")
+
+            data_ingestion = DataIngestion(
+                data_ingestion_config=self.data_ingestion_config,
+            )
+
             data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
-            logging.info("Got the train_set and test_set from s3")
-            logging.info("Exited the start_data_ingestion method of TrainPipeline Class")
-        except Exception as e:
-            raise CustomException(e, sys)
-    
 
-    def run_pipeline(self)->None:
-        try:
-            logging.info("Entered the run_pipeline method of TrainPipeline class")
-            dta_ingestion_artifact: DataIngestionArtifact = self.start_data_ingestion()
-            logging.info("Exited the run_pipeline method of TrainPipeline class")
+            logging.info("Got the train_set and test_set from s3")
+
+            logging.info(
+                "Exited the start_data_ingestion method of TrainPipeline class"
+            )
+
+            return data_ingestion_artifact
 
         except Exception as e:
             raise CustomException(e, sys)
         
+
+    
+
+    def start_data_transformation(
+        self, data_ingestion_artifact: DataIngestionArtifact
+    ) -> DataTransformationArtifact:
+        logging.info(
+            "Entered the start_data_transformation method of TrainPipeline class"
+        )
+
+        try:
+            data_transformation = DataTransformation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_transformation_config=self.data_transformation_config,
+            )
+
+            data_transformation_artifact = (
+                data_transformation.initiate_data_transformation()
+            )
+
+            logging.info(
+                "Exited the start_data_transformation method of TrainPipeline class"
+            )
+
+            return data_transformation_artifact
+
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+
+    
+
+    def run_pipeline(self) -> None:
+        logging.info("Entered the run_pipeline method of TrainPipeline class")
+
+        try:
+            data_ingestion_artifact: DataIngestionArtifact = self.start_data_ingestion()
+            data_transformation_artifact: DataTransformationArtifact = (
+                self.start_data_transformation(
+                    data_ingestion_artifact=data_ingestion_artifact
+                )
+            )
+            logging.info("Exited the run_pipeline method of TrainPipeline class")
+
+        except Exception as e:
+            raise CustomException(e, sys)

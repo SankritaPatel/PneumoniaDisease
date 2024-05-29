d1ad669f2e47872fa852cfe6db98f0cabf3e6af6
Sankrita Patel <sankrita6960@outlook.com>
2024-05-29 23:05:09 +0530

    model pusher constant added

--- src/pneumoniaDisease/entity/config_entity.py
+++ src/pneumoniaDisease/entity/config_entity.py
@@ -67,3 +67,10 @@ class ModelEvaluationConfig:
         self.optimizer_params:dict = {"lr":0.01, "momentum":0.8}
         
 
+@dataclass
+class ModelPusherConfig:
+    def __init__(self):
+        self.bentoml_model_name: str = BENTOML_MODEL_NAME
+        self.bentoml_service_name:str = BENTOML_SERVICE_NAME
+        self.train_transforms_key:str = TRAIN_TRANSFORMS_KEY
+        self.bentoml_ecr_image: str = BENTOML_ECR_URI
